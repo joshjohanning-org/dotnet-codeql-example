6e3674846b5af56adc4de1d5141e57f18f03e7cb
Josh Johanning <joshjohanning@github.com>
2024-04-24 20:48:51 -0500

    add more code [skip ci]

--- ReadingTime6.Web/Program.cs
+++ ReadingTime6.Web/Program.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Security.Cryptography;
 
 namespace ReadingTime6.Web
 {
     public class Program
     {
+    public static byte[] encryptString()
+        {
+            SymmetricAlgorithm serviceProvider = new DESCryptoServiceProvider();
+            byte[] key = { 16, 22, 240, 11, 18, 150, 192, 21 };
+            serviceProvider.Key = key;
+            ICryptoTransform encryptor = serviceProvider.CreateEncryptor();
+            String message = "Hello World";
+            byte[] messageB = System.Text.Encoding.ASCII.GetBytes(message);
+            return encryptor.TransformFinalBlock(messageB, 0, messageB.Length);
+        }
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
